cf4fdcdf6b94d6db1107d2b858fb9b869e8cba5e
priyohartono <yoyok.priyohartono@bmkg.go.id>
2026-02-20 02:38:48 +0000

    Setup Update

--- app/robots.ts
+++ app/robots.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next';
 
 export default function robots(): MetadataRoute.Robots {
-  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://stametsamarinda.bmkg.go.id';
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://stamet-samarinda.devbmkg.my.id';
 
   return {
     rules: {
